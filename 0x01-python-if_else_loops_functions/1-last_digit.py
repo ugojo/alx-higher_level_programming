45cdf031a02244d9d989fdeb1fecd9d12a85a993
obi ugochukwu <obiugochukwu003@gmail.com>
2023-06-07 09:40:00 -0700

    1-last_digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+num = abs(number) % 10
+if(num > 5):
+    print("Last digit of {} is {}  and is greater than 5".format(number, num))
+elif(num == 0):
+    print("last digit of {} is {} and is 0".format(number, num))
+elif(num < 6 & num != 0):
+    print("last digit of {} is {} and is less than 6 and not 0".format(number, num))
