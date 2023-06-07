5255f359deecb4a78aabcde7e53020befaf061f5
obi ugochukwu <obiugochukwu003@gmail.com>
2023-06-07 15:17:59 -0700

    5-print_comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for n in range(100):
-    if(n <=9):
-        print("0{}".format(n), end=", ")
-    elif(n == 99):
-        print("{}".format(n))
+    if n < 99:
+        print("{:02}".format(n), end=", ")
     else:
-        print("{}".format(n), end=", ")
+        print("{:02}".format(n))
