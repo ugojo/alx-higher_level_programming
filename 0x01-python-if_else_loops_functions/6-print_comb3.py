3b762be3775b40fe470377eb2a55ceb4dc0ff165
obi ugochukwu <obiugochukwu003@gmail.com>
2023-06-08 04:24:36 -0700

    6-print_comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for  n in range(90):
     if(n % 10 > n / 10):
-        print("{:02d}".format(n), end=", ")
-    elif(n == 89):
-        print("{:02d}".format(n))
+        if(n == 89):
+            print("{:02d}".format(n))
+        else:
+            print("{:02d}".format(n), end=", ")
