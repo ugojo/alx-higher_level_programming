a2511d31c17690e9f85f1601940b783017c9955a
obi ugochukwu <obiugochukwu003@gmail.com>
2023-06-08 04:32:25 -0700

    1-last_digit_update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,6 @@ if(num > 5):
     print("Last digit of {} is {} and is greater than 5".format(number, num))
 elif(num == 0):
     print("Last digit of {} is {} and is 0".format(number, num))
-elif(num < 6 & num != 0):
+elif(num < 6 & num < 0):
+    num = -num
     print("Last digit of {} is {} and is less than 6 and not 0".format(number, num))
