3de5b88701ca4f92225fb87397843400d0711e8d
obi ugochukwu <obiugochukwu003@gmail.com>
2023-06-08 16:11:09 -0700

    1-calculation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add
-    from calculator_1 import sub
-    from calculator_1 import mul
-    from calculator_1 import div
+    from calculator_1 import add, div, mul, sub
     a = 10
     b = 5
-    sums = add(a, b)
-    print("{} + {} = {}".format(a, b, sums))
-    subt =  sub(a, b)
-    print("{} - {} = {}".format(a, b, subt))
-    multi = mul(a, b)
-    print("{} * {} = {}".format(a, b, multi))
-    divi = div(a, b)
-    print("{} / {} = {}".format(a, b, divi))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
