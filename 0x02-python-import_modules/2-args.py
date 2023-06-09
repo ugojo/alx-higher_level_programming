4e1e8cede385cf30a426aad5ec03354afd6fcf53
obi ugochukwu <obiugochukwu003@gmail.com>
2023-06-08 19:31:28 -0700

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argv = sys.argv[1:]
-    argc = len(argv)
+    argvs = sys.argv[1:]
+    argc = len(argvs)
     if argc == 0:
-        print("0 arguments")
-    elif argc == 1:
-        print("1 argument")
+        print("0 arguments.")
     else:
-        print("{} arguments".format(argc))
+        print("{} arguments:".format(argc))
 
-        for i, arg in enumerate(argv, start=1):
+        for i, arg in enumerate(argvs, start=1):
             print("{}: {}".format(i, arg))
