efdb9302756e3d01c6668aa0de9f875a3197d26f
obi ugochukwu <obiugochukwu003@gmail.com>
2023-06-13 16:58:51 -0700

    4-new_in_list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
     else:
-        new  = [x for x in  my_list]
+        new  = [x for x in my_list]
         new[idx] = element
         return (new)
