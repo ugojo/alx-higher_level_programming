5be584ec700d8e63fe198c6325900fe30a66388c
obi ugochukwu <obiugochukwu003@gmail.com>
2023-06-14 08:52:24 -0700

    6-print_sorted_dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-     order_keys = sorted(a_dictionary.keys())
-    for key in order_keys:
-        value = a_dictionary[key]
-        print("{}: {}".format(key, value))
+    ord_list = sorted(a_dictionary.keys())
+
+    for key in ord_list:
+        val = a_dictionary[key]
+        print("{}: {}".format(key, val)
