7a1f88cfb36da2867b73a8282bc275d6cf6f45f3
obi ugochukwu <obiugochukwu003@gmail.com>
2023-06-14 08:12:56 -0700

    5-number_keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     num_keys = 0
-    list_keys = list(a_dictionary.key())
+    list_keys = list(a_dictionary.keys())
 
     for i in list_keys:
-        num_key += 1
-    return(num_key)
+        num_keys += 1
+    return(num_keys)
