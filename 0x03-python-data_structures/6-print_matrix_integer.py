a269340044007006cad097f9a2602b61fdedc82a
obi ugochukwu <obiugochukwu003@gmail.com>
2023-06-13 17:59:58 -0700

    6-print_matrix_integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[])):
+            if j != 0:
+                print(" ", end='')
+            print("{}".format(matrix[i][j]), end="")
+        print()
