e4259c99845456f47e13ac8739d72945d2b4d62c
obi ugochukwu <obiugochukwu003@gmail.com>
2023-06-14 17:49:14 -0700

    10-best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-     if not a_dictionary:
-        return (None)
-
-    return (max(a_dictionary, key=a_dictionary.get))
+    if not a_dictionary:
+        return None
+    max_score = float('-inf')
+    best_key = None
+    for key, value in a_dictionary.items():
+        if value > max_score:
+            max_score = value
+            best_key = key
+    return best_key
